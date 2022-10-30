e4a49ea95048f62e72ae7bfce5426d6680c1edce
nt54hamnghi <hamnghi250699@gmail.com>
2022-10-29 21:07:56 -0400

    add two point-generating functions

--- delicacy/svglib/point.py
+++ delicacy/svglib/point.py
@@ -1,7 +1,5 @@
 import random
 from math import cos, radians, sin
-from random import Random
-from types import ModuleType
 from typing import NamedTuple
 
 
@@ -16,23 +14,51 @@ class Point(NamedTuple):
         return cls(x, y)
 
     @classmethod
-    def from_polar_degree(cls, radius, theta):
-        theta = radians(theta)
-        return cls.from_polar_radian(radius, theta)
+    def from_polar_degree(cls, radius, degree):
+        return cls.from_polar_radians(radius, radians(degree))
 
     @classmethod
     def randpoint(
         cls,
-        xlim: tuple[int, int],
-        ylim: tuple[int, int],
+        xlim: tuple[int, int] = (0, 512),
+        ylim: tuple[int, int] = (0, 512),
         fixed_x: bool = False,
         fixed_y: bool = False,
-        rng: Random | None = None,
     ) -> "Point":
 
-        _rd: ModuleType | Random = random if rng is None else rng
-
-        x = _rd.choice(xlim) if fixed_x else _rd.randint(*xlim)
-        y = _rd.choice(ylim) if fixed_y else _rd.randint(*ylim)
+        x = random.choice(xlim) if fixed_x else random.randint(*xlim)
+        y = random.choice(ylim) if fixed_y else random.randint(*ylim)
 
         return cls(x, y)
+
+
+def rand_points(
+    num: int,
+    xlim: tuple[int, int],
+    ylim: tuple[int, int],
+    seed: int | None = None,
+):
+    if seed is not None:
+        random.seed(seed)
+
+    yield from (Point.randpoint(xlim, ylim) for _ in range(num))
+
+
+def rand_bounded_points(
+    num: int,
+    xlim: tuple[int, int],
+    ylim: tuple[int, int],
+    seed: int | None = None,
+):
+    """
+    generate random points whose x and y are randomly bounded to the limit
+    """
+
+    if seed is not None:
+        random.seed(seed)
+
+    fxs = random.choices([True, False], k=num)  # fixed flags for x
+    fys = random.choices([True, False], k=num)  # fixed flags for y
+
+    for _, fx, fy in zip(range(num), fxs, fys):
+        yield Point.randpoint(xlim, ylim, fx, fy)
