2ca1f22a7f6efd6e6760c61c65afb829cddf1b06
nt54hamnghi <hamnghi250699@gmail.com>
2022-11-16 19:31:46 -0500

    retire unuse code

--- delicacy/svglib/elements/peripheral/point.py
+++ delicacy/svglib/elements/peripheral/point.py
@@ -1,7 +1,5 @@
-import random
-from itertools import product
 from math import cos, radians, sin
-from typing import Iterator, NamedTuple
+from typing import NamedTuple
 
 
 class Point(NamedTuple):
@@ -17,66 +15,3 @@ class Point(NamedTuple):
     @classmethod
     def from_polar_degree(cls, radius, degree):
         return cls.from_polar_radians(radius, radians(degree))
-
-    @classmethod
-    def randpoint(
-        cls,
-        xlim: tuple[int, int] = (0, 512),
-        ylim: tuple[int, int] = (0, 512),
-        fixed_x: bool = False,
-        fixed_y: bool = False,
-    ) -> "Point":
-
-        x = random.choice(xlim) if fixed_x else random.randint(*xlim)
-        y = random.choice(ylim) if fixed_y else random.randint(*ylim)
-
-        return cls(x, y)
-
-
-def rand_points(
-    num: int,
-    xlim: tuple[int, int],
-    ylim: tuple[int, int],
-    seed: int | None = None,
-) -> Iterator[Point]:
-    if seed is not None:
-        random.seed(seed)
-
-    yield from (Point.randpoint(xlim, ylim) for _ in range(num))
-
-
-def rand_bounded_points(
-    num: int,
-    xlim: tuple[int, int],
-    ylim: tuple[int, int],
-    seed: int | None = None,
-) -> Iterator[Point]:
-    """
-    generate random points whose x and y are randomly bounded to the limit.
-    """
-    if seed is not None:
-        random.seed(seed)
-
-    options = tuple(product([True, False], repeat=2))
-
-    for fdx, fdy in random.choices(options, k=num):
-        yield Point.randpoint(xlim, ylim, fdx, fdy)
-
-
-def rand_fixed_points(
-    num: int,
-    xlim: tuple[int, int],
-    ylim: tuple[int, int],
-    seed: int | None = None,
-) -> Iterator[Point]:
-
-    """
-    generate random points whose either x and y must be bounded to the limit.
-    """
-    if seed is not None:
-        random.seed(seed)
-
-    options = tuple(filter(sum, product([True, False], repeat=2)))
-
-    for fdx, fdy in random.choices(options, k=num):
-        yield Point.randpoint(xlim, ylim, fdx, fdy)
--- tests/svglib/test_point.py
+++ tests/svglib/test_point.py
@@ -1,14 +1,8 @@
 from math import pi
-from unittest.mock import patch
 
 import pytest
 
-from delicacy.svglib.elements.peripheral.point import (
-    Point,
-    rand_bounded_points,
-    rand_fixed_points,
-    rand_points,
-)
+from delicacy.svglib.elements.peripheral.point import Point
 
 
 def test_create_point():
@@ -44,89 +38,3 @@ def test_create_point_from_polar_radians(radius, theta, expected):
 def test_create_point_from_polar_degree(radius, theta, expected):
     x, y = Point.from_polar_degree(radius, theta)
     assert Point(round(x), round(y)) == Point(*expected)
-
-
-def test_rand_point():
-    p = Point.randpoint()
-    assert 0 <= p.x <= 512
-    assert 0 <= p.y <= 512
-
-
-@pytest.mark.parametrize(
-    ("fixed_x", "fixed_y"),
-    ((True, False), (False, True), (True, True)),
-    ids=("fixed x", "fixed y", "fixed both"),
-)
-def test_rand_point_fixed(fixed_x, fixed_y):
-    x, y = Point.randpoint(
-        fixed_x=fixed_x,
-        fixed_y=fixed_y,
-    )
-
-    if all((fixed_x, fixed_y)):
-        assert x == 0 or x == 512
-        assert y == 0 or y == 512
-    elif fixed_x:
-        assert x == 0 or x == 512
-        assert 0 <= y <= 512
-    else:
-        assert 0 <= x <= 512
-        assert y == 0 or y == 512
-
-
-@pytest.mark.parametrize("seed", tuple(range(3)))
-def test_rand_points_with_seed(seed):
-    xlim = ylim = (0, 512)
-    first = rand_points(3, xlim, ylim, seed=seed)
-    second = rand_points(3, xlim, ylim, seed=seed)
-
-    assert list(first) == list(second)
-
-
-@patch("delicacy.svglib.elements.peripheral.point.random.choices")
-def test_rand_bounded_points(mock_choices):
-    mock_choices.return_value = [(True, True), (True, False), (False, False)]
-    xlim = ylim = (0, 512)
-    points = rand_bounded_points(3, xlim, ylim)
-
-    def onbound(n, lim):
-        start, end = lim
-        return n == start or n == end
-
-    x, y = next(points)
-    assert onbound(x, xlim) and onbound(y, ylim)
-
-    x, y = next(points)
-    assert onbound(x, xlim) and not onbound(y, ylim)
-
-    x, y = next(points)
-    assert not onbound(x, xlim) and not onbound(y, ylim)
-
-
-@pytest.mark.parametrize("seed", tuple(range(3)))
-def test_rand_bounded_points_with_seed(seed):
-    xlim = ylim = (0, 512)
-    first = rand_bounded_points(3, xlim, ylim, seed=seed)
-    second = rand_bounded_points(3, xlim, ylim, seed=seed)
-
-    assert list(first) == list(second)
-
-
-def test_rand_fixed_points():
-    xlim = ylim = (0, 512)
-    points = list(rand_bounded_points(3, xlim, ylim))
-
-    def onbound(n, lim):
-        start, end = lim
-        return n == start or n == end
-
-    assert all(onbound(x, xlim) or onbound(y, ylim) for x, y in points)
-
-
-@pytest.mark.parametrize("seed", tuple(range(3)))
-def test_rand_fixed_points_with_seed(seed):
-    xlim = ylim = (0, 512)
-    first = rand_fixed_points(3, xlim, ylim, seed=seed)
-    second = rand_fixed_points(3, xlim, ylim, seed=seed)
-
-    assert list(first) == list(second)
