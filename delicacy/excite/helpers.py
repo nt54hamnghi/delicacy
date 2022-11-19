e51235cafc249bae606294ab53201f5f69775876
nt54hamnghi <hamnghi250699@gmail.com>
2022-11-18 20:59:57 -0500

    refactor and add tests for helpers

--- delicacy/excite/helpers.py
+++ delicacy/excite/helpers.py
@@ -1,8 +1,14 @@
 from collections.abc import Iterator
-from itertools import product
+from itertools import accumulate, product, repeat
+from operator import mul
 from random import Random
 
-from delicacy.svglib.elements.element import ExtendedElement, defs, group
+from delicacy.svglib.elements.element import (
+    ExtendedElement,
+    WrappingElement,
+    defs,
+    group,
+)
 from delicacy.svglib.elements.peripheral.point import Point
 from delicacy.svglib.elements.peripheral.style import Fill, Stroke
 from delicacy.svglib.elements.peripheral.transform import Transform
@@ -25,67 +31,6 @@ def sorted_randspace(
     yield from (
         rng.randint(start, (start := start + min_space)) for _ in range(k)
     )
-
-
-def fade(
-    rng: Random,
-    element: ExtendedElement,
-    color: str,
-    num: int = 3,
-    location: Point = Point(0, 0),
-    scale: float | None = None,
-    rotate: float | None = None,
-    spread: tuple[int, int] = (20, 25),
-    fading_scale: float = 0.7,
-) -> ExtendedElement:
-
-    scale = rng.randint(15, 25) / 100 if scale is None else scale
-    rotate = rng.randint(0, 360) if rotate is None else rotate % 360
-
-    eid = str(id(element))
-    defs_element = defs(group(element, id=eid))
-
-    uses = []
-    fill = Fill(color="none")
-    direction = rng.choices([-1, 1], k=2)
-
-    width = 10.0 if num <= 1 else 20.0
-    opacity = 1.0
-
-    for i in range(num):
-        distance = rng.choices(range(*spread), k=2)
-        new_location = ((i * ds * dr) for ds, dr in zip(distance, direction))
-
-        # mypy can't understand that __init__ is injected by attrs
-        use = Use(eid, new_location)  # type: ignore
-        use.apply_styles(Stroke(color, opacity, width), fill)
-
-        width *= fading_scale
-        opacity *= fading_scale
-
-        uses.append(use)
-
-    faded = group(defs_element, *uses)
-    transform = Transform().translate(*location).scale(scale).rotate(rotate)
-    faded.add_transform(transform)
-
-    return faded
-
-
-def make_elm(side: int = 120, option: str = "rec") -> ExtendedElement:
-    match option:  # noqa
-        case "rec":
-            return Rectangle.make_rectangle(0, 0, side, side)
-        case "tri":
-            return ETriangle(side=side)
-        case "cir":
-            return Circle.make_circle(side // 2, 0, 0)
-        case "xsh":
-            return XShape(length=side)
-        case _:
-            raise ValueError(
-                f'option: {option} is not one of "rec", "tri", "cir", "xsh"'
-            )
 
 
 def linear_plane(
@@ -115,3 +60,70 @@ def rand_plane(
 
     plane = linear_plane(xrange, yrange, xk, yk)
     yield from (coord for coord in plane if rng.random() < rate)
+
+
+def make_elm(side: int = 120, option: str = "rec") -> ExtendedElement:
+    match option:  # noqa
+        case "rec":
+            return Rectangle.make_rectangle(0, 0, side, side)
+        case "tri":
+            return ETriangle(side=side)
+        case "cir":
+            return Circle.make_circle(side // 2, 0, 0)
+        case "xsh":
+            return XShape(length=side)
+        case _:
+            raise ValueError(
+                f'option: {option} is not one of "rec", "tri", "cir", "xsh"'
+            )
+
+
+def spreadit(
+    rng: Random,
+    spread: tuple[float, float],
+    k: int = 3,
+):
+    direction = rng.choices((-1, 1), k=2)
+    spread = range(*spread)
+
+    for i, (x, y) in enumerate(repeat(direction, times=k)):
+        dx, dy = rng.choices(spread, k=2)
+        yield i * x * dx, i * y * dy
+
+
+def fade(
+    rng: Random,
+    element: ExtendedElement,
+    color: str,
+    scale: float,
+    num: int = 3,
+    location: Point = Point(0, 0),
+    rotate: float | None = None,
+    spread: tuple[int, int] = (15, 25),
+    fading_scale: float = 0.8,
+) -> ExtendedElement:
+
+    opacity = 1
+    width = 10 if num <= 1 else 20
+    rotate = rng.randint(0, 360) if rotate is None else rotate % 360
+    fill = Fill(color="none")
+
+    eid = str(id(element))
+    defs_element = defs(group(element, id=eid))
+
+    faded = WrappingElement("g")
+    faded.append(defs_element)
+
+    for loc in spreadit(rng, spread, k=num):
+        stroke = Stroke(color, opacity, width)
+        use = Use(eid, loc)  # type: ignore
+        use.apply_styles(stroke, fill)
+        faded.append(use)
+
+        width *= fading_scale
+        opacity *= fading_scale
+
+    transform = Transform().translate(*location).scale(scale).rotate(rotate)
+    faded.add_transform(transform)
+
+    return faded
---
+++ tests/excite/test_helpers.py
@@ -0,0 +1,156 @@
+import random
+from itertools import product, repeat
+from random import Random
+from unittest import mock
+import pytest
+
+from delicacy.excite.helpers import (
+    fade,
+    linear_plane,
+    rand_plane,
+    sorted_randspace,
+    make_elm,
+    spreadit,
+)
+from delicacy.svglib.elements.peripheral.transform import Transform
+from delicacy.svglib.elements.shapes import Circle, Path, Rectangle
+
+
+@pytest.mark.parametrize(
+    ("seed", "start", "stop", "k"),
+    product((0, 1), (0, 32), (256, 512), range(5, 30, 10)),
+)
+def test_sorted_randspace(seed, start, stop, k):
+    s1 = list(sorted_randspace(Random(seed), start, stop, k))
+    s2 = list(sorted_randspace(Random(seed), start, stop, k))
+
+    # reproducibility
+    assert s1 == s2
+    assert s1 == sorted(s1)
+    assert set(s1).issubset(set(range(start, stop + 1)))
+
+
+@pytest.mark.parametrize("option", "rec tri cir xsh".split())
+def test_make_elm(option):
+    elm = make_elm(option=option)
+    if option == "rec":
+        assert isinstance(elm, Rectangle)
+    elif option == "cir":
+        assert isinstance(elm, Circle)
+    else:
+        assert isinstance(elm, Path)
+
+
+def test_make_elm_fail():
+    with pytest.raises(ValueError):
+        make_elm(option="")
+
+
+@pytest.mark.parametrize(
+    ("k", "_range"),
+    tuple(
+        product(
+            (10, 20),
+            product(
+                map(lambda x: pow(2, x), range(2)),
+                map(lambda x: pow(2, x), range(8, 10)),
+            ),
+        )
+    ),
+)
+class TestPlane:
+    def test_linear_plane(self, k, _range):
+        plane = linear_plane(_range, _range, k, k * 2)
+        plane = tuple(plane)
+
+        assert len(plane) == k * k * 2
+
+    @pytest.mark.parametrize("random_returned", (0, 1))
+    @mock.patch("delicacy.excite.helpers.Random")
+    def test_rand_plane(self, mock_rng, k, _range, random_returned):
+        mock_rng.random.return_value = random_returned
+        plane = rand_plane(mock_rng, _range, _range, k, k * 2, 0.5)
+        plane = tuple(plane)
+
+        expected = k * k * 2
+
+        if random_returned == 0:
+            expected *= 1
+        elif random_returned == 1:
+            expected *= 0
+
+        assert len(plane) == expected
+
+    @pytest.mark.parametrize("rate", (0.25, 0.75, 1))
+    def test_rand_plane_reproducible(self, k, _range, rate):
+        seed = int(rate * 100)
+        plane0 = rand_plane(Random(seed), _range, _range, k, k * 2, rate)
+        plane1 = rand_plane(Random(seed), _range, _range, k, k * 2, rate)
+
+        assert all(p0 == p1 for p0, p1 in zip(plane0, plane1))
+
+
+@pytest.mark.parametrize("rate", (-1, 0, 1.1))
+def test_rand_plane_fail(rate):
+    with pytest.raises(ValueError):
+        tuple(rand_plane(Random(0), rate=rate))
+
+
+@pytest.mark.parametrize(
+    ("k", "spread"),
+    tuple(
+        product(
+            range(2, 4),
+            ((5, 20), (10, 20), (15, 20)),
+        ),
+    ),
+)
+class TestSpreadIt:
+    @pytest.mark.parametrize(
+        "direction",
+        tuple(product((-1, 1), repeat=2)),
+    )
+    @mock.patch("delicacy.excite.helpers.Random")
+    def test_spread_it(self, mock_rng, k, spread, direction):
+        mock_rng.choices.side_effect = [direction] + list(
+            repeat(spread, times=k)
+        )
+        spr = spreadit(mock_rng, spread, k)
+        spr = tuple(spr)
+
+        assert len(spr) == k
+
+        expected = [
+            (i * spread[0] * direction[0], i * spread[1] * direction[1])
+            for i in range(k)
+        ]
+
+        assert tuple(expected) == spr
+
+    @pytest.mark.parametrize("seed", range(3))
+    def test_spread_it_reproducible(self, spread, k, seed):
+        spr0 = spreadit(Random(seed), spread, k)
+        spr1 = spreadit(Random(seed), spread, k)
+
+        assert all(s0 == s1 for s0, s1 in zip(spr0, spr1))
+
+
+@pytest.mark.parametrize(
+    ("option", "num", "scale", "location"),
+    product(
+        ("rec", "cir", "tri", "xsh"),
+        range(2, 4),
+        (0.2, 0.3),
+        product((0, 1), repeat=2),
+    ),
+)
+def test_fade(option, num, scale, location):
+    elm = make_elm(option=option)
+    faded = fade(
+        random, elm, "black", scale, num=num, location=location, rotate=90
+    )
+
+    assert len(faded.base) == num + 1
+
+    expected = Transform().translate(*location).scale(scale).rotate(90)
+    assert faded.base.attrib["transform"] == expected()
