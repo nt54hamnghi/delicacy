c8e7ac4043681b0cb7e387f0c5a293a4d858acd8
nt54hamnghi <hamnghi250699@gmail.com>
2022-11-28 19:07:08 -0500

    create api

--- delicacy/main.py
+++ delicacy/main.py
@@ -1,10 +1,37 @@
+from enum import Enum
+from io import BytesIO
+
+from fastapi import FastAPI, Query
+from fastapi.responses import Response
+
 from delicacy.generate import generate
-from delicacy.excite.excite import ExAid
+from delicacy.excite.excite import ExAid, Genm, ParaDX
 
 
-def main(phrase: str):
-    generate(phrase, ExAid)
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main("")
+class MakerType(str, Enum):
+    exaid = "exaid"
+    genm = "genm"
+    paradx = "paradx"
+
+
+@app.get("/make/{maker_type}")
+async def make(maker_type: MakerType, phrase: str = Query(max_length=32)):
+
+    match maker_type:
+        case maker_type.exaid:
+            maker = ExAid
+        case maker_type.genm:
+            maker = Genm
+        case maker_type.paradx:
+            maker = ParaDX
+        case _:
+            raise ValueError("invalid maker_typeType")
+
+    img = generate(phrase, maker)
+
+    with BytesIO() as imgbytes:
+        img.save(imgbytes, "png")
+        return Response(content=imgbytes.getvalue(), media_type="image/png")
